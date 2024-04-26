cbb01d838902eb832cac384717a2eacb0f2a8673
Wilfredo Amoretti <wamoretti@yahoo.com>
2024-04-25 22:07:57 -0500

    Cambios del día 2024-04-25

--- app/page.tsx
+++ app/page.tsx
@@ -11,22 +11,22 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-      {/* Add Hero Images Here */}
-      <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
-      <Image
-        src="/hero-mobile.png"
-        width={560}
-        height={620}
-        className="block md:hidden"
-        alt="Screenshot of the dashboard project showing mobile version"
-      />
-    </div>
+        {/* Add Hero Images Here */}
+        <Image
+          src="/hero-desktop.png"
+          width={1000}
+          height={760}
+          className="hidden md:block"
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+        <Image
+          src="/hero-mobile.png"
+          width={560}
+          height={620}
+          className="block md:hidden"
+          alt="Screenshot of the dashboard project showing mobile version"
+        />
+      </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         
